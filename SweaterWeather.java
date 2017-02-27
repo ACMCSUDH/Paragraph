5e6b01412f5a038dacc180327b8cf7e98ff4157b
Thomas Mejia <tommym147@gmail.com>
2017-02-26 19:42:11 -0800

    added first part of lyrics

--- SweaterWeather.java
+++ SweaterWeather.java
@@ -1,5 +1,16 @@
 public class SweaterWeather {
 	public static main (String [] args) {
 		System.out.println("Sweater Weather by the Neighbourhood");
+		
+		// Adding first paragraph of lyrics
+		System.out.println("And all I am is a man");
+		System.out.println("I want the world in my hands");
+		System.out.println("I hate the beach");
+		System.out.println("But I stand in California with my toes in the sand");
+		System.out.println("Use the sleeves on my sweater");
+		System.out.println("Lets have an adventure");
+		System.out.println("Head in the clouds but my gravity centered");
+		System.out.println("Touch my neck and I'll touch yours");
+		System.out.println("You in the little high waisted shorts");
 	}
 }
