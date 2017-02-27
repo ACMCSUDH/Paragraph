40de29b0f63a6a88d90dce234911fd37891ce409
Alvarotony17 <tzulalvaro@gmail.com>
2017-02-26 19:58:14 -0800

    adding second paragraph

--- SweaterWeather.java
+++ SweaterWeather.java
@@ -1,5 +1,16 @@
 public class SweaterWeather {
 	public static main (String [] args) {
 		System.out.println("Sweater Weather by the Neighbourhood");
+		
+		System.out.println("oh, she knows what i think about");
+		System.out.println("and what i think about ");
+		System.out.println("one love , two mouths");
+		System.out.println("one love, two house");
+		System.out.println("no shirst no blouse");
+		System.out.println("just us your find out");
+		System.out.println("nothingj i really want to tell you already know");
+		System.out.println(" cause it too cold wo");
+		System.out.println("next for you here");
+
 	}
 }
