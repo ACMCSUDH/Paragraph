b8f1a200951df3b2e3b2d7e13edd7983ac8c108f
edtn44 <estherdt44@gmail.com>
2017-12-30 20:56:52 -0800

    Update FavChorus.java

--- FavChorus.java
+++ FavChorus.java
@@ -1,13 +1,13 @@
-public class FavChours {
+public class FavChorus {
   public static void main(String[] args) {
-    System.out.println("Fellow Feeling by Porter Robinson");
+    	System.out.println("Fellow Feeling by Porter Robinson");
     
-    System.out.println("Chorus:");
-		System.out.println("Let me explain");
-		System.out.println("This ugliness, this cruelty, this repulsiveness");
-		System.out.println("It will all die out");
-		System.out.println("And now I cry for all that is beautiful");
-		System.out.println(":')");
+    	System.out.println("Chorus:");
+    	System.out.println("Let me explain");
+    	System.out.println("This ugliness, this cruelty, this repulsiveness");
+     	System.out.println("It will all die out");
+	System.out.println("And now I cry for all that is beautiful");
+	System.out.println(":')");
 		
 		
 
